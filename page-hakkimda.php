083fabb6f280b2c7d93f8e0be7962088e564ad64
benfiratkaya <benfiratkaya@gmail.com>
2021-02-28 13:15:07 +0300

    Hakkımda sayfasına avatar eklendi.

--- page-hakkimda.php
+++ page-hakkimda.php
@@ -1,9 +1,16 @@
 <?php get_header(); ?>
 <section class="section">
   <div class="container">
-    <h1 class="header-top-text">Hakkımda</h1>
-    <p class="header-bottom-text">Ben kimim ve neler yaparım gibi bir çok sorunun yanıtı</p>
     <?php while (have_posts()) : the_post(); ?>
+      <div class="row">
+        <div class="col">
+          <h1 class="header-top-text">Hakkımda</h1>
+          <p class="header-bottom-text">Ben kimim ve neler yaparım gibi bir çok sorunun yanıtı</p>
+        </div>
+        <div class="col-auto d-none d-md-block">
+          <img class="avatar avatar-2x rounded-circle" src="<?php echo esc_url(get_avatar_url(get_the_author_id())); ?>" alt="Hakkımda" loading="lazy">
+        </div>
+      </div>
       <div class="row">
         <div class="col-12">
           <?php get_template_part('template-parts/contents/content', 'page'); ?>
